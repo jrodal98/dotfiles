97f8bf1e33ad7a9be235a16b093e63abef97e941
Jacob Rodal <jrodal@meta.com>
2026-01-26 10:40:13 -0800

    support for happy wrapper

--- zsh/.jrodal_zsh_utils/wezterm/init.sh
+++ zsh/.jrodal_zsh_utils/wezterm/init.sh
@@ -52,7 +52,7 @@ __notify_precmd() {
     # Skip interactive commands that are expected to run long
     local cmd_first="${__cmd_command%% *}"
     case "$cmd_first" in
-      nvim|vim|vi|claude|ssh|tmux|slo|yazi|sudoedit|dmt|sls|sld)
+      nvim|vim|vi|claude|ssh|tmux|slo|yazi|sudoedit|dmt|sls|sld|happy)
         __cmd_start_time=
         __cmd_command=
         return
